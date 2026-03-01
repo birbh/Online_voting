79e4c8d266726b5a391864084d8a226018e429eb
biraj <birajbh65@gmail.com>
2026-03-01 16:42:48 +0545

    style(config): normalize suspect motive punctuation

--- js/config.js
+++ js/config.js
@@ -3,28 +3,28 @@ const suspectOptions = [
     id: 's1',
     name: 'Lydia Mercer',
     role: '1st Year',
-    motive: '23 yrs old - knows the campus well',
+    motive: '23 yrs old - knows the campus well.',
     photoUrl: 'images/lydia.jpg'
   },
   {
     id: 's2',
     name: 'Marco Vale',
     role: 'Final Year',
-    motive: '27 yrs old - lots of pressure to finish',
+    motive: '27 yrs old - lots of pressure to finish.',
     photoUrl: 'images/marco.jpg'
   },
   {
     id: 's3',
     name: 'Noah Kade',
     role: 'Lab Assistant',
-    motive: '32 yrs old - knows his way around the lab',
+    motive: '32 yrs old - knows his way around the lab.',
     photoUrl: 'images/noah.jpg'
   },
   {
     id: 's4',
     name: 'Theo Ash',
     role: '3rd Year',
-    motive: '25 yrs old - has some weird friends',
+    motive: '25 yrs old - has some weird friends.',
     photoUrl: 'images/theo.jpg'
   }
 ];
